5adb6015442acef8e58a8fa21b047d9f7c39d80a
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2018-11-16 23:55:59 -0500

    HW2

---
+++ DecisionTree.py
@@ -0,0 +1,120 @@
+
+class Node:
+    """ base (master) class for nodes """
+    def __init__(self, name, cost):
+        """
+        :param name: name of this node
+        :param cost: cost of visiting this node
+        """
+
+        self.name = name
+        self.cost = cost
+
+    def get_expected_cost(self):
+        """ abstract method to be overridden in derived classes
+        :returns expected cost of this node """
+
+
+class ChanceNode(Node):
+
+    def __init__(self, name, cost, future_nodes, probs):
+        """
+        :param name: name of this node
+        :param cost: cost of visiting this node
+        :param future_nodes: (list) future nodes connected to this node
+        :param probs: (list) probability of future nodes
+        """
+
+        Node.__init__(self, name, cost)
+        self.futureNodes = future_nodes
+        self.probs = probs
+
+    def get_expected_cost(self):
+        """
+        :return: expected cost of this chance node
+        E[cost] = (cost of visiting this node)
+                  + sum_{i}(probability of future node i)*(E[cost of future node i])
+        """
+
+        # expected cost initialized with the cost of visiting the current node
+        exp_cost = self.cost
+
+        # go over all future nodes
+        i = 0
+        for node in self.futureNodes:
+            # increment expected cost by
+            # (probability of visiting this future node) * (expected cost of this future node)
+            exp_cost += self.probs[i]*node.get_expected_cost()
+            i += 1
+
+        return exp_cost
+
+
+class TerminalNode(Node):
+
+    def __init__(self, name, cost):
+        """
+        :param name: name of this node
+        :param cost: cost of visiting this node
+        """
+
+        Node.__init__(self, name, cost)
+
+    def get_expected_cost(self):
+        """
+        :return: cost of this visiting this terminal node
+        """
+        return self.cost
+
+
+class DecisionNode(Node):
+
+    def __init__(self, name, cost, future_nodes):
+        """
+        :param name: name of this node
+        :param cost: cost of visiting this node
+        :param future_nodes: (list) future nodes connected to this node
+        (assumes that future nodes can only be chance or terminal nodes)
+        """
+
+        Node.__init__(self, name, cost)
+        self.futureNode = future_nodes
+
+    def get_expected_costs(self):
+        """ returns the expected costs of future nodes
+        :return: a dictionary of expected costs of future nodes with node names as dictionary keys
+        """
+
+        # a dictionary to store the expected cost of future nodes
+        exp_costs = dict()
+        # go over all future nodes
+        for node in self.futureNode:
+            # add the expected cost of this future node to the dictionary
+            exp_costs[node.name] = self.cost + node.get_expected_cost()
+
+        return exp_costs
+
+
+#######################
+# See figure DT3.png (from the project menu) for the structure of this decision tree
+########################
+
+# create the terminal nodes
+T1 = TerminalNode('T1', 10)
+T2 = TerminalNode('T2', 20)
+T3 = TerminalNode('T3', 30)
+T4 = TerminalNode('T4', 40)
+T5 = TerminalNode('T5', 50)
+
+# create C2
+C2 = ChanceNode('C2', 15, [T1, T2], [0.1, 0.9])
+# create C1
+C1 = ChanceNode('C1', 0, [C2, T3], [0.4, 0.6])
+# create C3
+C3 = ChanceNode('C3', 2, [T4, T5], [0.2, 0.8])
+
+# create D1
+D1 = DecisionNode('D1', 0, [C1, C3])
+
+# print the expect cost of C1
+print(D1.get_expected_costs())
