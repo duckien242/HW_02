c2579d941ade730c03ced906a474af6f519c5774
Savanah Russ <savanah.russ@yale.edu>
2019-01-25 14:05:35 -0500

    HW2 Savanah Solutions

--- Debugging/SumRatio.py
+++ Debugging/SumRatio.py
@@ -1,3 +1,4 @@
+import math as math
 
 class SumRatio:
     def __init__(self, x, y):
@@ -8,17 +9,23 @@ class SumRatio:
         self.x = x
         self.y = y
 
+        assert len(x) == len(y), 'x and y should have the same number of elements'
+
     def get_sum_ratio(self):
         """
         :return: x_1/y_1 + x_2+y_2 + ...
         """
 
         # number of ratios to calculate
-        num_of_elements = len(self.x_)
+        num_of_elements = len(self.x)
+
 
         # sum all ratios
         sum = 0
         for i in range(1, num_of_elements):
-            sum = self.x[i]/self.y[i]
-
+            try:
+                sum = self.x[i]/self.y[i]
+            except ZeroDivisionError:
+                print('Warning: Division by 0 in calculating the ratio in position' + str(i))
+                return math.nan
         return sum
