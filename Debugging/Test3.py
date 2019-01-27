f9173d1fbfa4b0fdca0902aa00cd5b914b6408c8
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2019-01-27 09:24:07 -0500

    Test 3 updated

--- Debugging/Test3.py
+++ Debugging/Test3.py
@@ -10,6 +10,6 @@ sum_ratio = Cls.SumRatio(x=x, y=y)
 result = sum_ratio.get_sum_ratio()
 
 if result is correct_sum_ratio:
-    print('Test 2 passed. Sum of ratios = ', result)
+    print('Test 3 passed. Sum of ratios = ', result)
 else:
-    print('Test 2 failed. Calculated sum of ratios: ', result, 'Correct sum of ratios: ', correct_sum_ratio)
+    print('Test 3 failed. Calculated sum of ratios: ', result, 'Correct sum of ratios: ', correct_sum_ratio)
